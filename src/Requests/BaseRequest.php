f43ac0a51be771d0b8277f322622a83245bdfbd0
jianzi0307 <jianzi0307@qq.com>
2020-08-17 22:32:37 +0800

    fix

--- src/Requests/BaseRequest.php
+++ src/Requests/BaseRequest.php
@@ -2,10 +2,17 @@
 
 namespace Jianzi\Repository\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class BaseRequest extends FormRequest
 {
+    /**
+     * 参数错误
+     */
+    const _PARAMS_ERROR_ = 422;
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -14,5 +21,18 @@ class BaseRequest extends FormRequest
     public function authorize()
     {
         return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw (new HttpResponseException(response()->json([
+            'code' => $this->getFaiedValidationCode(),
+            'msg' => $validator->errors()->first()
+        ], 200)));
+    }
+
+    protected function getFaiedValidationCode()
+    {
+        return self::_PARAMS_ERROR_;
     }
 }
